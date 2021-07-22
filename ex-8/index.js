da2ecde156343ebc08bd73711ef1b26cbde9e5d9
Nikolay Golovin <nik.golovin83@gmail.com>
2021-07-22 14:31:23 +0600

    задание 8 выполнено

--- ex-8/index.js
+++ ex-8/index.js
@@ -1,12 +1,27 @@
-const userForm = document.querySelector('.user-form');
-const userBtn = document.querySelector(`.btn`);
+const userForm = document.querySelector(`.user-form`);
+const userBtn = document.querySelector(`.user-form-btn`);
+const userInput = document.querySelector(`.user-input`);
+
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+let guesNumber = getRandom(1, 10);
+
+const usersTry = () => {     
+   if(guesNumber === (+userInput.value)) {
+      alert(`Good Work`)      
+   }
+   else {
+      alert(`Not matched`);
+      return guesNumber = getRandom(1, 10);            
+   }  
+};
+
+userBtn.addEventListener(`click`, usersTry);
+
 const getAnswer = (evt) => {
-   evt.preventDefault();
-   
-}
+   evt.preventDefault();   
+};
 
 userForm.addEventListener(`submit`, getAnswer);
 
