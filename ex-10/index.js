dafdd3d8a048f74c3033bdb3a79c460611fd9586
Nikolay Golovin <nik.golovin83@gmail.com>
2021-07-22 19:06:08 +0600

    задание 8 WIP

---
+++ ex-10/index.js
@@ -0,0 +1,7 @@
+const firstNumber = document.getElementById(`first-number`);
+const secondNumber = document.getElementById(`second-number`);
+const multiplyBtn = document.querySelector(`.multiply-btn`);
+const divideBtn = document.querySelector(`.divide-btn`);
+const calcResult = document.querySelector(`.calc-result`);
+
+
