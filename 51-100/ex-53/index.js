4d8c9d607461e6169e2c25da594f0ee5f9f873a6
Nikolay Golovin <nik.golovin83@gmail.com>
2021-08-08 20:42:06 +0600

    задание 53 добавил переменную range

--- 51-100/ex-53/index.js
+++ 51-100/ex-53/index.js
@@ -1,11 +1,11 @@
 // Write a JavaScript program to check whether the characters a and b are separated by exactly 3 places anywhere (at least once) in a given string.
 const sentence = `indialib`;
-
+let range = 3;
 
 const findSeparetedChr = (str) => {
     const searchElemOne = str.indexOf(`a`);
     const searchElemTwo = str.indexOf(`b`);
-    return searchElemOne + 3 === searchElemTwo || searchElemTwo - 3 === searchElemOne;
+    return searchElemOne + range === searchElemTwo || searchElemTwo - range === searchElemOne;
 }
 
 console.log(findSeparetedChr(sentence));
